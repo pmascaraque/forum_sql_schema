b0136ef60565fd6f78ac39fd46d0ad68fb9f3357
Pablo Moreno <ssm.pablo@gmail.com>
2022-08-14 11:05:25 +0200

    add created at column & feed query

--- feed.sql
+++ feed.sql
@@ -1,1 +1,17 @@
 --post feed (tables and data in ./gen_data.sql)
+
+--add created_at column
+alter table posts 
+	add column created_at date
+default now() - (random() * interval '100 days');
+
+select * from posts;
+
+
+--feed
+select p.created_at, p.title,
+	substr(p.body, 1, 30), u.first_name from posts p
+inner join users u on p."creatorId" = u.id
+order by created_at desc
+offset 0
+limit 20;
